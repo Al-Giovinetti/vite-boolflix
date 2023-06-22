d1418b4d311def311e34deee7148b08d2f6f7cbc
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-22 15:03:24 +0200

    passate alle SingleCard le img

--- src/components/ContainerFilmList.vue
+++ src/components/ContainerFilmList.vue
@@ -23,12 +23,14 @@ export default{
         :originaltitle = film.original_title
         :language = film.original_language
         :vote = film.vote_average
+        :image = film.poster_path
           />
         <SingleCard v-for="serie in myserielist" 
         :title = serie.name
         :originaltitle = serie.original_name
         :language = serie.original_language
-        :vote = serie.vote_average />
+        :vote = serie.vote_average 
+        :image = serie.poster_path />
     </div>
     
 </template>
