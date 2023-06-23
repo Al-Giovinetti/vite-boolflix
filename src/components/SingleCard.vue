ee64ab83dfa71ea058e985665ca96dedf2cf367d
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-23 12:59:35 +0200

    hover sulle card

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -36,35 +36,53 @@ export default{
       <img v-if="image != null" :src="getImage(image)" :alt="title+ 'poster'">
       <img v-else src="https://cdn1.vectorstock.com/i/1000x1000/13/55/background-in-a-matrix-style-green-random-numbers-vector-39461355.jpg" alt="random background">
     </div>
-    <div class="box-text">
-      <p>{{ title }}</p>
-      <p>{{ originaltitle }}</p>
-    </div>
-    <!-- Per far la bandiera basta mettre fi fi-(2 iniziali stato ) - vedi demo -->
-    <div class="box-language">
-      <span v-if="NationsWithoutFlag.includes(language)" class="fi fi-xx">  </span>
-      <span v-else :class="`fi fi-${language}`">  </span>
-    </div>
-    <div class="box-star">
-      <span>{{ newVote }}</span>
-      <i v-for="n in newVote" class="fa-solid fa-star"></i>
-      <i v-for="n in getDifference(newVote)" class="fa-regular fa-star"></i>
+    <div class="back-card">
+      <div class="box-text">
+        <p>{{ title }}</p>
+        <p>{{ originaltitle }}</p>
+      </div>
+      <!-- Per far la bandiera basta mettre fi fi-(2 iniziali stato ) - vedi demo -->
+      <div class="box-language">
+        <span v-if="NationsWithoutFlag.includes(language)" class="fi fi-xx">  </span>
+        <span v-else :class="`fi fi-${language}`">  </span>
+      </div>
+      <div class="box-star">
+        <span>{{ newVote }}</span>
+        <i v-for="n in newVote" class="fa-solid fa-star"></i>
+        <i v-for="n in getDifference(newVote)" class="fa-regular fa-star"></i>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import "/node_modules/flag-icons/css/flag-icons.min.css";
-
 div.card{
   margin: 1rem;
   text-align: center;
-  width: 180px;
+  width: 300px;
+  height: 500px;
+  position: relative;
+    &:hover div.box-img{
+      opacity: 20%;
+    }
+    &:hover div.back-card{
+      display: block;
+    }
+}
+
+div.back-card{
+  display: none;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
 }
 
 div.box-img{
-  height: 250px;
-  width: 180px;
+  height: 500px;
+  width: 300px;
+  
   img{
     height: 100%;
     width: 100%;
@@ -77,7 +95,6 @@ div.box-text{
 }
 div.box-language{
   color: white;
-  background-color: black;
   padding: 0.5rem;
   .fi{
   width: 70px;
